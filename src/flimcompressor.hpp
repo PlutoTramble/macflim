98695ccabcc7b261813cea35dbab979456b4351f
PlutoTramble <Cedric.A.T@protonmail.com>
2025-04-06 12:01:15 -0400

    Move flimcompressor's classes

--- src/flimcompressor.hpp
+++ src/flimcompressor.hpp
@@ -51,117 +51,12 @@ public:
                 source{s}, ticks{t}, video{v}, audio{a}, result{r} {}
     };
 
-private:
-    size_t W_;
-    size_t H_;
-
-    const std::vector<image> &images_;
-    const std::vector<sound_frame_t> &audio_;
-    const double fps_;
-    std::vector<subtitle> subtitles_;
-
-    std::vector<frame> frames_;
-
-public:
-    flimcompressor( size_t W, size_t H, const std::vector<image> &images, const std::vector<sound_frame_t> &audio, double fps, const std::vector<subtitle> &subtitles ) : W_{W}, H_{H}, images_{images}, audio_{audio}, fps_{fps}, subtitles_{subtitles} {}
-
-    const std::vector<frame> &get_frames() const { return frames_; }
-
-    bool progress_ = true;
-
     struct codec_spec
     {
         uint8_t signature;
         double penality;
         std::shared_ptr<compressor> coder;
     };
-
-    static std::vector<std::string> split( const std::string s, const std::string delimiter )
-    {
-        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-        std::string token;
-        std::vector<std::string> res;
-
-        while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos)
-        {
-            token = s.substr (pos_start, pos_end - pos_start);
-            pos_start = pos_end + delim_len;
-            res.push_back (token);
-        }
-
-        res.push_back (s.substr (pos_start));
-        return res;
-    }
-
-    static codec_spec make_codec( const std::string &spec_string, size_t W, size_t H )
-    {
-        auto spec_array = split( spec_string, ":" );
-        auto name = spec_array[0];
-        std::string parameters_string = "";
-
-        if (spec_array.size()>1)
-            parameters_string = spec_array[1];
-
-        codec_spec spec;
-        spec.signature = 0x00;
-        spec.penality = 1;
-        spec.coder = std::make_shared<null_compressor>( W, H );
-
-        if (name=="z16")
-        {
-            spec.signature = 0x01;
-            spec.penality = 0.45;
-            spec.coder = std::make_shared<vertical_compressor<uint16_t>>( W, H, uint16_ruler::ruler );
-        }
-        else if (name=="z32")
-        {   
-            spec.signature = 0x02;
-            spec.penality = 1.00;
-            spec.coder = std::make_shared<vertical_compressor<uint32_t>>( W, H, uint32_ruler::ruler );
-        }
-        else if (name=="z32old")
-        {   
-            static bit_ruler<uint32_t> br32;
-            spec.signature = 0x02;
-            spec.penality = 1.00;
-            spec.coder = std::make_shared<vertical_compressor<uint32_t>>( W, H, br32 );
-        }
-        else if (name=="invert")
-        {
-            spec.signature = 0x03;
-            spec.penality = 1.00;
-            spec.coder = std::make_shared<invert_compressor>( W, H );
-        }
-        else if (name=="lines")
-        {   
-            spec.signature = 0x04;
-            spec.penality = 1.00;
-            spec.coder = std::make_shared<copy_line_compressor>( W, H );
-        }
-        else if (name=="null")
-        {   
-            spec.signature = 0x00;
-            spec.penality = 1.00;
-            spec.coder = std::make_shared<null_compressor>( W, H );
-        }
-        else
-        {
-            std::clog << "Unknown codec : [" << name << "]\n";
-            throw "Unknown codec";
-        }
-
-        for (auto &param_string:split(parameters_string,","))
-        {
-            auto v = split( param_string, "=" );
-            std::string pname = v[0];;
-            std::string pvalue = "";
-            if (v.size()>1)
-                pvalue = v[1];
-            spec.coder->set_parameter( pname, pvalue );
-        }
-
-        return spec;
-    }
 
     struct DitheringParameters
     {
@@ -181,17 +76,17 @@ public:
     {
         size_t W_, H_;              //  Width and height of the generated image
         image dithered_image_;      //  The currently dithered image
-                                    //  The initial image defines the size of all future images
+        //  The initial image defines the size of all future images
 
 
         const DitheringParameters dp_;
 
     public:
         Ditherer( const image &inital_image, const DitheringParameters &dp ) :
-            W_{ inital_image.W() },
-            H_{ inital_image.H() },
-            dithered_image_{ W_, H_ },
-            dp_{dp}
+                W_{ inital_image.W() },
+                H_{ inital_image.H() },
+                dithered_image_{ W_, H_ },
+                dp_{dp}
         {
             //  Initial dithered image is black, we dither to whatever the initial image is
             dither( inital_image );
@@ -201,16 +96,16 @@ public:
         size_t H() const { return H_; }
 
         /// Dither the image according to the parameters
-        /// Passed 
+        /// Passed
         void dither( const image &img )
         {
             image resized_image( W_, H_ );   //  note: was 512x342
             copy( resized_image, img, dp_.bars_ );
 
-                //  We filter the image of the "right size", for things like corners, etc...
+            //  We filter the image of the "right size", for things like corners, etc...
             image filtered_image = filter( resized_image, dp_.filters_.c_str() );
 
-            image dithered_image( W_, H_ ); //  The next dithered image
+            image dithered_image( W_, H_ ); //  The extract_video_frame dithered image
 
             if (dp_.dither_==image::error_diffusion)
                 error_diffusion( dithered_image, filtered_image, dithered_image_, dp_.stability_, *get_error_diffusion_by_name( dp_.error_algorithm_ ), dp_.error_bleed_, dp_.error_bidi_ );
@@ -219,8 +114,8 @@ public:
             else
                 throw "Unknown dithering option";
 
-                //  note: used to be done *after* (ie: in a local copy)
-           round_corners( dithered_image );
+            //  note: used to be done *after* (ie: in a local copy)
+            round_corners( dithered_image );
             ::watermark( dithered_image, dp_.watermark_ );
 
             // char buffer[1024];
@@ -229,7 +124,7 @@ public:
             // num++;
             // write_image( buffer, dithered_image );
 
-                //  The new dithered image is the previous one
+            //  The new dithered image is the previous one
             dithered_image_ = dithered_image;
         }
 
@@ -243,12 +138,12 @@ public:
     class SubtitleBurner
     {
         std::vector<subtitle> subtitles_;    //  The subtitles to burn
-            // #### Should be a pair of const_iterators
+        // #### Should be a pair of const_iterators
 
-        public:
+    public:
 
         SubtitleBurner( const std::vector<subtitle> &subtitles ) :
-            subtitles_{ subtitles }
+                subtitles_{ subtitles }
         {}
 
         //  Burn the subtitle for time into the image;
@@ -256,7 +151,7 @@ public:
         {
             if (subtitles_.size()>0)
             {
-                    //  Can do that way better with an iterator!
+                //  Can do that way better with an iterator!
                 if (time>=subtitles_.front().start)
                 {
                     if (time<subtitles_.front().stop)
@@ -279,17 +174,17 @@ public:
         const std::vector<uint8_t> data_;   //  Resulting data
         const double quality_;              //  Resulting quality
 
-        public:
+    public:
         EncodingResult(
-            const codec_spec &codec,
-            const framebuffer &current,
-            const framebuffer &target,
-            const size_t budget
-            ) :
-            codec_{ codec },
-            image_{ current },
-            data_{ codec_.coder->compress( image_, target,  budget*codec_.penality ) },
-            quality_{ image_.proximity( target ) }
+                const codec_spec &codec,
+                const framebuffer &current,
+                const framebuffer &target,
+                const size_t budget
+        ) :
+                codec_{ codec },
+                image_{ current },
+                data_{ codec_.coder->compress( image_, target,  budget*codec_.penality ) },
+                quality_{ image_.proximity( target ) }
         {
             // char buffer[1024];
             // static int num = 0;
@@ -387,8 +282,8 @@ public:
         };
 
         qhistogram<BucketCount> histo_;
-    
-        public:
+
+    public:
         CompressorHelper(
                 Ditherer& ditherer,
                 SubtitleBurner &subtitle_burner,
@@ -397,15 +292,15 @@ public:
                 const size_t byterate,
                 const std::vector<sound_frame_t> &audio,
                 const bool group
-                    ) :
-            ditherer_{ ditherer },
-            subtitle_burner_{ subtitle_burner },
-            current_fb_{ ditherer_.current() },
-            codecs_{ codecs },
-            fps_{ fps },
-            byterate_{ byterate },
-            audio_{ audio },
-            group_{ group }
+        ) :
+                ditherer_{ ditherer },
+                subtitle_burner_{ subtitle_burner },
+                current_fb_{ ditherer_.current() },
+                codecs_{ codecs },
+                fps_{ fps },
+                byterate_{ byterate },
+                audio_{ audio },
+                group_{ group }
         {
             //  blah
             current_tick_ = 0;
@@ -416,15 +311,15 @@ public:
         // Adds one image to the generated video, keep track of previous
         void add( const image &source )
         {
-                //  Dither the new image
+            //  Dither the new image
             ditherer_.dither( source );
             image dest = ditherer_.current();
             subtitle_burner_.burn_into( dest, in_fr_/fps_ );
 
-                //  True B&W packed image
+            //  True B&W packed image
             framebuffer fb{ dest };
 
-                //  Let's see how many ticks we have to display this image
+            //  Let's see how many ticks we have to display this image
             in_fr_++;
             size_t next_tick = ticks_from_frame( in_fr_, fps_ );
             size_t ticks = next_tick-current_tick_;
@@ -438,7 +333,7 @@ public:
 
             for (size_t i=0;i!=ticks;i+=local_ticks)
             {
-                    //  Add as much audio as we have for the local ticks
+                //  Add as much audio as we have for the local ticks
                 std::vector<uint8_t> audio;
                 for (size_t i=0;i!=local_ticks;i++)
                 {
@@ -451,26 +346,26 @@ public:
 // write_image( "/tmp/a.pgm", fb.as_image() );
 // write_image( "/tmp/b.pgm", dest );
 
-                    //  Compute the video budget?
+                //  Compute the video budget?
                 size_t video_budget = byterate_*local_ticks;
-               
-                    //  Encode within that budget with every codec
-                std::vector<EncodingResult> encoding_results;                
+
+                //  Encode within that budget with every codec
+                std::vector<EncodingResult> encoding_results;
                 std::transform(std::begin(codecs_), std::end(codecs_), std::back_inserter(encoding_results), [&]( auto &codec )-> EncodingResult
-                    { return EncodingResult(
+                { return EncodingResult(
                         codec,
                         current_fb_,
                         fb,
                         video_budget*codec.penality
-                    ); } );
+                ); } );
 
-                    //  Find the result with highest quality
+                //  Find the result with highest quality
                 auto best_result = std::max_element(encoding_results.begin(), encoding_results.end(), [](const EncodingResult& r1, const EncodingResult& r2) { return r1.quality() < r2.quality(); } );
 
 // write_image( "/tmp/img1.pgm", best_result->image().as_image() );
 // exit(0);
 
-                    //  Construct the frame with best video and audio
+                //  Construct the frame with best video and audio
                 frame f{ fb, local_ticks, best_result->get_video_encoded_data(), audio, best_result->image() };
 
                 frames_.push_back( f );
@@ -497,6 +392,111 @@ public:
 
         std::vector<frame> get_frames() const { return frames_; }
     };
+
+private:
+    size_t W_;
+    size_t H_;
+
+    const std::vector<image> &images_;
+    const std::vector<sound_frame_t> &audio_;
+    const double fps_;
+    std::vector<subtitle> subtitles_;
+
+    std::vector<frame> frames_;
+
+public:
+    flimcompressor( size_t W, size_t H, const std::vector<image> &images, const std::vector<sound_frame_t> &audio, double fps, const std::vector<subtitle> &subtitles ) : W_{W}, H_{H}, images_{images}, audio_{audio}, fps_{fps}, subtitles_{subtitles} {}
+
+    const std::vector<frame> &get_frames() const { return frames_; }
+
+    bool progress_ = true;
+
+    static std::vector<std::string> split( const std::string s, const std::string delimiter )
+    {
+        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+        std::string token;
+        std::vector<std::string> res;
+
+        while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos)
+        {
+            token = s.substr (pos_start, pos_end - pos_start);
+            pos_start = pos_end + delim_len;
+            res.push_back (token);
+        }
+
+        res.push_back (s.substr (pos_start));
+        return res;
+    }
+
+    static codec_spec make_codec( const std::string &spec_string, size_t W, size_t H )
+    {
+        auto spec_array = split( spec_string, ":" );
+        auto name = spec_array[0];
+        std::string parameters_string = "";
+
+        if (spec_array.size()>1)
+            parameters_string = spec_array[1];
+
+        codec_spec spec;
+        spec.signature = 0x00;
+        spec.penality = 1;
+        spec.coder = std::make_shared<null_compressor>( W, H );
+
+        if (name=="z16")
+        {
+            spec.signature = 0x01;
+            spec.penality = 0.45;
+            spec.coder = std::make_shared<vertical_compressor<uint16_t>>( W, H, uint16_ruler::ruler );
+        }
+        else if (name=="z32")
+        {   
+            spec.signature = 0x02;
+            spec.penality = 1.00;
+            spec.coder = std::make_shared<vertical_compressor<uint32_t>>( W, H, uint32_ruler::ruler );
+        }
+        else if (name=="z32old")
+        {   
+            static bit_ruler<uint32_t> br32;
+            spec.signature = 0x02;
+            spec.penality = 1.00;
+            spec.coder = std::make_shared<vertical_compressor<uint32_t>>( W, H, br32 );
+        }
+        else if (name=="invert")
+        {
+            spec.signature = 0x03;
+            spec.penality = 1.00;
+            spec.coder = std::make_shared<invert_compressor>( W, H );
+        }
+        else if (name=="lines")
+        {   
+            spec.signature = 0x04;
+            spec.penality = 1.00;
+            spec.coder = std::make_shared<copy_line_compressor>( W, H );
+        }
+        else if (name=="null")
+        {   
+            spec.signature = 0x00;
+            spec.penality = 1.00;
+            spec.coder = std::make_shared<null_compressor>( W, H );
+        }
+        else
+        {
+            std::clog << "Unknown codec : [" << name << "]\n";
+            throw "Unknown codec";
+        }
+
+        for (auto &param_string:split(parameters_string,","))
+        {
+            auto v = split( param_string, "=" );
+            std::string pname = v[0];;
+            std::string pvalue = "";
+            if (v.size()>1)
+                pvalue = v[1];
+            spec.coder->set_parameter( pname, pvalue );
+        }
+
+        return spec;
+    }
 
 
     void compress( double stability, size_t byterate, bool group, const std::string &filters, const std::string &watermark, const std::vector<codec_spec> &codecs, image::dithering dither, bool bars, const std::string error_algorithm, float error_bleed, bool error_bidi )
